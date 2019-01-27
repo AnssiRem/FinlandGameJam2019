3161b0a5ac28f1594a093a1a29dc4b8cb0133373
Remes Anssi TTK17SP <anssiremes@kamk.fi>
2019-01-27 13:34:36 +0200

    Add more assets

---
+++ Assets/Imported Assets/Koponen/Buddha.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buddha : MonoBehaviour
+{
+    public AudioSource AudioS;
+
+    private bool fired = false;
+    private FridgeDoor door;
+
+    private void Start()
+    {
+        door = GameObject.Find("Ovi").GetComponent<FridgeDoor>();
+    }
+
+    private void Update()
+    {
+        if (!fired && door.DoorState == FridgeDoor.State.Open)
+        {
+            AudioS.enabled = true;
+            AudioS.PlayOneShot(AudioS.clip);
+            fired = true;
+        }
+    }
+}
